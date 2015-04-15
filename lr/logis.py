4c67790de3123311f1f544807259070a9bf54bba
dawume <roberts512@sina.com>
2015-04-15 21:48:31 +0800

    fix

--- lr/logis.py
+++ lr/logis.py
@@ -1,71 +1,54 @@
 #!/bin/python
-from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-def loadDataSet() :
+def loadDataSet(dataFile):
 	dataMat = []; labelMat = []
-	f = open('testSet.txt')
-	for line in f :
-		fs = line.rstrip().split()
-		dataMat.append([1.0, float(fs[0]), float(fs[1])])
-		labelMat.append(int(fs[2]))
+	data = np.loadtxt(dataFile, delimiter = '\t')
+	dataMat = data[:, 0:-1]
+	labelMat = data[:, -1]
 	return dataMat, labelMat
 
-def sigmoid(num) :
-	return 1.0 / (1 + exp(-num))
-
-def grad(dataMat, labelMat) :
-	dataMatrix = mat(dataMat)
-	labelMatrix = mat(labelMat).transpose()
-	m,n = shape(dataMatrix)
-	alpha = 0.001
-	maxCycles = 500
-	weights = ones((n,1))
-	for k in xrange(maxCycles) :
-		h = sigmoid(dataMatrix * weights)
-		error = (labelMatrix - h)
-		weights = weights + alpha * dataMatrix.transpose() * error * sum((array(h) * (1 - array(h))))
-	return weights
+def sigmoid(num):
+	return 1.0 / (1 + np.exp(-num))
 
 def stocGrad(dataMat, labelMat) :
-	m, n = shape(dataMat)
-	weights = ones(n)
-	for k in range(200) :
-		for i in range(m) :
-			alpha = 4/(1.0 + k + i) + 0.01
-			h = sigmoid(sum(dataMat[i] * weights))
-			error = labelMat[i] - h
-			weights = weights + alpha * error * h * (1-h) * array(dataMat[i])
-	return mat(weights).T
+	pass
 
-def plotBestFit(wei) :
-	import matplotlib.pyplot as plt
-	weights = wei.getA()
-	dataMat, labelMat = loadDataSet()
-	dataArr = array(dataMat)
-	n = shape(dataArr)[0]
+def grad(dataMat, labelMat):
+	alpha = 0.001
+	theta = np.random.rand(3)
+	for i in np.arange(2000):
+		out = sigmoid(np.dot(data, theta))
+		J =  np.sum(- label * np.log(out) - (1 - label) * np.log(1 - out))
+		print J
+		theta_delta = np.dot(data.transpose(), out - label)
+		theta = theta - alpha * theta_delta
+		if i % 200 == 0:
+			plotBestFit(data, label, theta)
+
+def plotBestFit(dataMat, labelMat, weights) :
+	n = np.shape(dataMat)[0]
 	xcord1 = []; ycord1 = []
 	xcord2 = []; ycord2 = []
 	for i in range(n):
 		if int(labelMat[i]) == 1 :
-			xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+			xcord1.append(dataMat[i,0]); ycord1.append(dataMat[i,1])
 		else :
-			xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+			xcord2.append(dataMat[i,0]); ycord2.append(dataMat[i,1])
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
-	ax.scatter(xcord2, ycord2, s=30, c='green')
-	x = arange(-3.0, 3.0, 0.1)
-	y = (-weights[0]-weights[1]*x)/weights[2]
+	ax.scatter(xcord1, ycord1, s = 30, c = 'red', marker = 's')
+	ax.scatter(xcord2, ycord2, s = 30, c = 'green')
+	x = np.arange(-3.0, 3.0, 0.1)
+	y = (- weights[2] - weights[0] * x) / weights[1]
 	ax.plot(x,y)
 	plt.xlabel('X1'); plt.ylabel('X2')
 	plt.show()
 
 if __name__ == '__main__' :
-	dataMat, labelMat = loadDataSet()
-	weights = grad(dataMat, labelMat)
-	print weights
-
-	# plotBestFit(weights)
-	weights = stocGrad(dataMat, labelMat)
-	print weights
-	plotBestFit(weights)
+	dataMat, label = loadDataSet('../data/testSet.txt')
+	m,n = np.shape(dataMat)
+	data = np.hstack((dataMat, np.ones((m,1))))
+	grad(data, label)
+	
